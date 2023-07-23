0c4a3a3e12d31b7293907d395c0fbbe466edf76e
suparna <suparna72bundhate@gmail.com>
2023-07-23 22:03:22 +0530

    Login Screen
    worked on Login Screen

---
+++ app/src/main/java/in/ddw/carinfo/pages/LoginActivity.java
@@ -0,0 +1,47 @@
+package in.ddw.carinfo.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import in.ddw.carinfo.R;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText ed_mbno;
+    TextView tv_sendotp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ed_mbno = findViewById(R.id.ed_mbno);
+        tv_sendotp = findViewById(R.id.tv_sendotp);
+
+        tv_sendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonClick();
+            }
+        });
+
+    }
+
+    public void onButtonClick()
+    {
+        String mobileno = ed_mbno.getText().toString();
+
+        if(mobileno.equalsIgnoreCase(""))
+        {
+            ed_mbno.setError("Please Enter Mobile No");
+            ed_mbno.requestFocus();
+        }
+        else
+        {
+            //call api
+        }
+    }
+}
--- app/src/main/java/in/ddw/carinfo/pages/SplashActivity.java
+++ app/src/main/java/in/ddw/carinfo/pages/SplashActivity.java
@@ -2,6 +2,7 @@ package in.ddw.carinfo.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -17,6 +18,15 @@ public class SplashActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
 
     }
 
