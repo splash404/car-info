b296d563055f6195b87ae246b69bbee5179902d0
Ankit Sharma <as7250893@gmail.com>
2023-07-27 02:34:45 +0530

    <your_work_define>

--- app/src/main/java/in/ddw/carinfo/pages/LoginActivity.java
+++ app/src/main/java/in/ddw/carinfo/pages/LoginActivity.java
@@ -18,30 +18,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ed_mbno = findViewById(R.id.ed_mbno);
-        tv_sendotp = findViewById(R.id.tv_sendotp);
+//        ed_mbno = findViewById(R.id.ed_mbno);
+//        tv_sendotp = findViewById(R.id.tv_sendotp);
 
-        tv_sendotp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onButtonClick();
-            }
-        });
+//        tv_sendotp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onButtonClick();
+//            }
+//        });
 
     }
 
-    public void onButtonClick()
-    {
-        String mobileno = ed_mbno.getText().toString();
-
-        if(mobileno.equalsIgnoreCase(""))
-        {
-            ed_mbno.setError("Please Enter Mobile No");
-            ed_mbno.requestFocus();
-        }
-        else
-        {
-            //call api
-        }
-    }
+//    public void onButtonClick()
+//    {
+//        String mobileno = ed_mbno.getText().toString();
+//
+//        if(mobileno.equalsIgnoreCase(""))
+//        {
+//            ed_mbno.setError("Please Enter Mobile No");
+//            ed_mbno.requestFocus();
+//        }
+//        else
+//        {
+//            //call api
+//        }
+//    }
 }
