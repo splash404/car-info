d8468e8eb199c7b2cfaad0133b8884b4e4419251
Ankit Sharma <as7250893@gmail.com>
2023-07-30 14:34:02 +0530

    <your_work_define>

--- app/src/main/java/in/ddw/carinfo/pages/Home.java
+++ app/src/main/java/in/ddw/carinfo/pages/Home.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,8 @@ public class Home extends AppCompatActivity {
     LatestNewsAdapter latestNewsAdapter;
     RecyclerView rv_latestNews, rv_EVnews;
 
+    ImageView iv_profile,iv_setting;
+
     List<NewsModel> list = new ArrayList<>();
 
 
@@ -28,12 +33,34 @@ public class Home extends AppCompatActivity {
 
         findId();
         getList();
+        onClick();
+    }
+
+    private void onClick() {
+
+        iv_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iv_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void findId() {
 
         rv_latestNews = findViewById(R.id.rv_latestNews);
         rv_EVnews = findViewById(R.id.rv_EVnews);
+        iv_profile = findViewById(R.id.iv_profile);
+        iv_setting = findViewById(R.id.iv_setting);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(RecyclerView.HORIZONTAL);
@@ -64,8 +91,7 @@ public class Home extends AppCompatActivity {
                 "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
                 "27 July 2023", "By Admin"));
 
-        list.add(new NewsModel(R.drawable.carimg4,
-                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
-                "27 July 2023", "By Admin"));
+        list.add(new NewsModel(R.drawable.carimg4, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ", "27 July 2023", "By Admin"));
     }
+
 }
